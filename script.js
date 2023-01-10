1a206c12c158820373b5b3d92eeb46f37fed244e
YasinDehfuli <Ydehfuli@gmail.com>
2023-01-10 12:02:37 +0330

    FULL ANIMATIONAL NAVIGASHION (CODEPEN PROJECT)

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggle = document.getElementById('toggle')
+const nav = document.getElementById('nav')
+
+toggle.addEventListener('click', () => nav.classList.toggle('active'))
+
+// https://Github.com/YasinDehfuli
+// https://Codepen.io/YasinDehfuli
